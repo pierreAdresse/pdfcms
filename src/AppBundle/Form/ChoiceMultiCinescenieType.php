3746c127bf25b5913f87d17f1330e64b8718e491
Pierre <pierre.adresse@hotmail.fr>
2018-05-15 22:35:47 +0200

    Date en français sans le module intl

--- src/AppBundle/Form/ChoiceMultiCinescenieType.php
+++ src/AppBundle/Form/ChoiceMultiCinescenieType.php
@@ -17,6 +17,13 @@ class ChoiceMultiCinescenieType extends AbstractType
             ->add('cinescenies', ChoiceType::class, [
                 'choices' => $cinescenies,
                 'choice_label' => function($cinescenie, $key, $index) {
+                    $day   = $this->getDayFr($cinescenie->getDate());
+                    $month = $this->getMonthFr($cinescenie->getDate());
+                    $dayNum = $cinescenie->getDate()->format('j');
+                    $then   = $cinescenie->getDate()->format('Y à H:i');
+                    return "$day $dayNum $month $then";
+
+                    /*
                     $formatter = new \IntlDateFormatter(
                         'fr_FR',
                         \IntlDateFormatter::FULL,
@@ -27,6 +34,7 @@ class ChoiceMultiCinescenieType extends AbstractType
                     );
 
                     return ucfirst($formatter->format($cinescenie->getDate()));
+                    */
                 },
                 'expanded' => true,
                 'multiple' => true,
@@ -40,5 +48,74 @@ class ChoiceMultiCinescenieType extends AbstractType
         $resolver->setDefaults([
             'defaultCinescenies' => '',
         ]);
+    }
+
+    private function getDayFr($date)
+    {
+        switch ($date->format('w')) {
+            case 0:
+                return 'Dimanche';
+                break;
+            case 1:
+                return 'Lundi';
+                break;
+            case 2:
+                return 'Mardi';
+                break;
+            case 3:
+                return 'Mercredi';
+                break;
+            case 4:
+                return 'Jeudi';
+                break;
+            case 5:
+                return 'Vendredi';
+                break;
+            case 6:
+                return 'Samedi';
+                break;
+        }
+    }
+
+    private function getMonthFr($date)
+    {
+        switch ($date->format('n')) {
+            case 1:
+                return 'janvier';
+                break;
+            case 2:
+                return 'février';
+                break;
+            case 3:
+                return 'mars';
+                break;
+            case 4:
+                return 'avril';
+                break;
+            case 5:
+                return 'mai';
+                break;
+            case 6:
+                return 'juin';
+                break;
+            case 7:
+                return 'juillet';
+                break;
+            case 8:
+                return 'août';
+                break;
+            case 9:
+                return 'septembre';
+                break;
+            case 10:
+                return 'octobre';
+                break;
+            case 11:
+                return 'novembre';
+                break;
+            case 12:
+                return 'décembre';
+                break;   
+        }
     }
 }
